de5fd4ae7d19dd62097734fbde26d07672b0436b
Lawrence Tray <ltray99@gmail.com>
2021-11-28 19:09:15 +0000

    Implement map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './component/map'
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <Map />
+        </div>
       </header>
     </div>
   );
