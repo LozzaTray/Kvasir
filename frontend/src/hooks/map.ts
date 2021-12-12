6ff3b18c737abe2797b36861e73f985dc40fd83c
Lawrence Tray <ltray99@gmail.com>
2021-12-12 12:11:34 +0000

    Move tests around

--- frontend/src/hooks/map.ts
+++ frontend/src/hooks/map.ts
@@ -1,16 +1,10 @@
 import React from "react";
 import { createCustomEqual } from "fast-equals";
-import { isLatLngLiteral } from "@googlemaps/typescript-guards";
 
 
 const deepCompareEqualsForMaps = createCustomEqual(
     (deepEqual) => (a: any, b: any) => {
-        if (
-            isLatLngLiteral(a) ||
-            a instanceof google.maps.LatLng ||
-            isLatLngLiteral(b) ||
-            b instanceof google.maps.LatLng
-        ) {
+        if (a instanceof google.maps.LatLng || b instanceof google.maps.LatLng) {
             return new google.maps.LatLng(a).equals(new google.maps.LatLng(b));
         }
 
