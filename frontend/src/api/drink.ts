a68112ff57b30602e263a594a95a3bc96ba69656
Lawrence Tray <ltray99@gmail.com>
2021-12-28 12:18:16 +0000

    Spin out into json

--- frontend/src/api/drink.ts
+++ frontend/src/api/drink.ts
@@ -1,32 +1,9 @@
 import { IDrink } from "../model/drink";
-
-const drinks = [
-    {
-        venue: "St John's College Bar",
-        lat: 52.20805855420469,
-        lng: 0.11630382846607584,
-        beverage: "Tetley's",
-        pence: 215
-    },
-    {
-        venue: "The Mitre",
-        lat: 52.20888571694211,
-        lng: 0.11814968298960132,
-        beverage: "Nicholson's Pale Ale",
-        pence: 420
-    },
-    {
-        venue: "The Pickerel",
-        lat: 52.20996368667585, 
-        lng: 0.11618023558240591,
-        beverage: "Foster's",
-        pence: 370
-    }
-]
+import * as drinksList from "./dummy/drinks.json";
 
 const getDrinks = (): IDrink[] => {
     // TODO: Hook up with api once built
-    return drinks;
+    return drinksList.drinks;
 }
 
 export { getDrinks }
