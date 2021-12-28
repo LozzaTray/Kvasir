49a71f5c656d496829cff39fd680e0d987bf6395
Lawrence Tray <ltray99@gmail.com>
2021-12-28 12:20:54 +0000

    Add custom marker icon to map (#12)
    * Scaffold .env.example
    
    * Add a marker
    
    * Refactor svg
    
    * Perfect icon
    
    * Implement using Google Map React
    
    * Sort out icons
    
    * remove duplicate svg
    
    * Update manifest
    
    * Move pints to cambridge
    
    * Newlines in env files
    
    * Spin out into json
    
    * Newlines

---
+++ frontend/src/api/drink.ts
@@ -0,0 +1,9 @@
+import { IDrink } from "../model/drink";
+import * as drinksList from "./dummy/drinks.json";
+
+const getDrinks = (): IDrink[] => {
+    // TODO: Hook up with api once built
+    return drinksList.drinks;
+}
+
+export { getDrinks }
--- frontend/src/component/map.tsx
+++ frontend/src/component/map.tsx
@@ -1,92 +1,33 @@
-import React from "react";
-import { Status, Wrapper } from "@googlemaps/react-wrapper";
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
 import config from "../config";
-import { useDeepCompareEffectForMaps } from "../hooks/map";
-import { Marker } from "./marker";
+import Drink from "./drink";
+import { getDrinks } from "../api/drink";
+
+// adapted from https://levelup.gitconnected.com/reactjs-google-maps-with-custom-marker-ece0c7d184c4
 
 
-interface MapProps extends google.maps.MapOptions {
-	style: { [key: string]: string };
-	onClick?: (e: google.maps.MapMouseEvent) => void;
-	onIdle?: (map: google.maps.Map) => void;
+const SimpleMap: React.FC = () => {
+  const [center, setCenter] = useState({
+    lat: 52.20805855420469,
+    lng: 0.11630382846607584,
+  });
+  const [zoom, setZoom] = useState(16);
+
+  const drinks = getDrinks();
+
+  return (
+    <div style={{ height: '80vh', width: '100vw' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: config.googleMapsApiKey }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+        yesIWantToUseGoogleMapApiInternals
+      >
+        {drinks.map((drink, idx) => <Drink key={`drink-${idx}`} {...drink} />)}
+      </GoogleMapReact>
+    </div>
+  );
 }
 
-
-const MapWrapper: React.VFC = () => {
-	const render = (status: Status) => {
-		switch (status) {
-			case Status.LOADING:
-				return <h1>Loading...</h1>;
-			case Status.FAILURE:
-				return <h1>Failed...</h1>;
-			default:
-				return <h1>Unknown...</h1>
-		}
-	};
-
-	const [zoom, setZoom] = React.useState(12);
-	const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
-		lat: 51.515,
-		lng: -0.111,
-	});
-
-	const onIdle = (m: google.maps.Map) => {
-		console.log("onIdle");
-		setZoom(m.getZoom()!);
-		setCenter(m.getCenter()!.toJSON());
-	};
-
-	const locations = [
-		{
-			lat: 51.515,
-			lng: -0.111
-		},
-		{
-			lat: 51.515,
-			lng: -0.121
-		}
-	]
-
-	return <Wrapper apiKey={config.googleMapsApiKey} render={render}>
-		<Map style={{ width: "100hh", height: "80vh" }} zoom={zoom} center={center} onIdle={onIdle}>
-			{locations.map((location) => <Marker position={location} />)}
-		</ Map>
-	</Wrapper>
-}
-
-const Map: React.FC<MapProps> = ({
-	onClick,
-	onIdle,
-	children,
-	style,
-	...options
-}) => {
-	const ref = React.useRef<HTMLDivElement>(null);
-	const [map, setMap] = React.useState<google.maps.Map>();
-
-	React.useEffect(() => {
-		if (ref.current && !map) {
-			setMap(new window.google.maps.Map(ref.current, {}));
-		}
-	}, [ref, map]);
-
-	useDeepCompareEffectForMaps(() => {
-		if (map) {
-			map.setOptions(options);
-		}
-	}, [map, options]);
-
-	return (
-		<>
-			<div ref={ref} style={style} />
-			{React.Children.map(children, (child) => {
-				if (React.isValidElement(child)) {
-					// set the map prop on the child component
-					return React.cloneElement(child, { map });
-				}
-			})}
-		</>
-	);
-};
-
-export default MapWrapper;
+export default SimpleMap;
