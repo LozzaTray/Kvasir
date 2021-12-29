7c3e18468cba15264402265c88f0150f45bde62c
Lawrence Tray <ltray99@gmail.com>
2021-12-29 17:56:39 +0000

    Revert to old linter config

--- frontend/src/component/drink.tsx
+++ frontend/src/component/drink.tsx
@@ -14,7 +14,7 @@ const Drink: React.FC<IDrink> = (props) => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <g>
-          <g fill="rgb(246, 193, 1)" stroke="rgb(0, 0, 0)" stroke-width="6px">
+          <g fill="rgb(246, 193, 1)" stroke="rgb(0, 0, 0)" strokeWidth="6px">
             <path d="M 0 -80 C 20 -90, 60 -70, 80 -90 C 90 -100, 40 -140, 90 -220  L -90 -220 C -40 -140, -90 -100, -80 -90 C -60 -70, -20 -90, 0 -80 Z" />
             <path d="M 0 0 C 20 -200, 150 -200, 100 -350 L -100 -350 C -150 -200, -20 -200, 0 0" />
           </g>
@@ -22,16 +22,16 @@ const Drink: React.FC<IDrink> = (props) => {
         <path
           fill="rgb(255, 255, 255)"
           stroke="rgb(0, 0, 0)"
-          stroke-width="6px"
+          strokeWidth="6px"
           d="M 100 -350 c -20 -60 -180 -60 -200 0 Z"
         />
         <text
           fill="rgb(0, 0, 0)"
-          font-family="Arial, sans-serif"
-          font-size="90px"
+          fontFamily="Arial, sans-serif"
+          fontSize="90px"
           x="0"
           y="-250"
-          text-anchor="middle"
+          textAnchor="middle"
         >
           {(props.pence / 100).toFixed(2)}
         </text>
