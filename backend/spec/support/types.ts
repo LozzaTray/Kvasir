e092cf7b8389326269c7e33dccd263f509b5406d
Lawrence Tray <ltray99@gmail.com>
2021-12-30 21:14:05 +0000

    Add PubDrink test

--- backend/spec/support/types.ts
+++ backend/spec/support/types.ts
@@ -1,9 +1,11 @@
 import { Response } from "supertest";
 import { IUser } from "entities/User";
+import { IPubDrink } from "entity/PubDrink";
 
 export interface IResponse extends Response {
     body: {
         users: IUser[];
+        drinks: IPubDrink[];
         error: string;
     };
 }
