7c391dce56a82388fbaaf1c048c37a175b706602
Lawrence Tray <ltray99@gmail.com>
2021-12-30 17:19:42 +0000

    Scaffold express app

---
+++ backend/src/daos/User/UserDao.mock.ts
@@ -0,0 +1,57 @@
+import { IUser } from "entities/User";
+import { IUserDao } from "./UserDao";
+import MockDaoMock from "../MockDb/MockDao.mock";
+
+const getRandomInt = (): number => {
+  return Math.floor(Math.random() * 1_000_000_000_000);
+};
+
+class UserDao extends MockDaoMock implements IUserDao {
+  public async getOne(email: string): Promise<IUser | null> {
+    const db = await super.openDb();
+    for (const user of db.users) {
+      if (user.email === email) {
+        return user;
+      }
+    }
+    return null;
+  }
+
+  public async getAll(): Promise<IUser[]> {
+    const db = await super.openDb();
+    return db.users;
+  }
+
+  public async add(user: IUser): Promise<void> {
+    const db = await super.openDb();
+    user.id = getRandomInt();
+    db.users.push(user);
+    await super.saveDb(db);
+  }
+
+  public async update(user: IUser): Promise<void> {
+    const db = await super.openDb();
+    for (let i = 0; i < db.users.length; i++) {
+      if (db.users[i].id === user.id) {
+        db.users[i] = user;
+        await super.saveDb(db);
+        return;
+      }
+    }
+    throw new Error("User not found");
+  }
+
+  public async delete(id: number): Promise<void> {
+    const db = await super.openDb();
+    for (let i = 0; i < db.users.length; i++) {
+      if (db.users[i].id === id) {
+        db.users.splice(i, 1);
+        await super.saveDb(db);
+        return;
+      }
+    }
+    throw new Error("User not found");
+  }
+}
+
+export default UserDao;
