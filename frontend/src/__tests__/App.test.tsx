ced332ff22e0c7a6d0ebcc045bd5fe77b12e9cdb
Lawrence Tray <ltray99@gmail.com>
2022-08-02 21:47:26 +0100

    K-26 Replace CRA with Vite (#27)
    * Scaffold vite project
    
    * K-26: Fix react version issues
    
    * K-26: Remove CRA project

--- frontend/src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-test("renders learn react link", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Pint Map/i);
-    expect(linkElement).toBeInTheDocument();
-});
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || "",
-    mockBackend: process.env.REACT_APP_MOCK_BACKEND || false,
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY || "",
+    mockBackend: import.meta.env.VITE_MOCK_BACKEND || false,
 };
